8ffc759999044f70fbcabfa6424473dbccbd715b
youswim <youswim96@gmail.com>
2023-08-27 20:06:32 +0900

    init

---
+++ src/test/java/reactive/reactivestreams/ReactiveStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package reactive.reactivestreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
