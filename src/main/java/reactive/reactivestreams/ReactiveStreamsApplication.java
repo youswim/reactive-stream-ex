5a29422009d62c1b127021ce9392d9bbf13d3f2e
youswim <youswim96@gmail.com>
2023-08-27 20:05:34 +0900

    init

---
+++ src/main/java/reactive/reactivestreams/ReactiveStreamsApplication.java
@@ -0,0 +1,13 @@
+package reactive.reactivestreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveStreamsApplication.class, args);
+    }
+
+}
